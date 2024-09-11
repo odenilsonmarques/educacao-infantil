c1595d6e74c7f2a577dfe619875b7aa8803597bd
odenilson marques <odmarquesgm@gmail.com>
2024-09-11 16:06:17 -0300

    creating files that make up the theme

---
+++ front-page.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <h1 class="">hello world</h1>
+        </div>
+        <div class="col-6">
+            <h1 class="">hello world</h1>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
