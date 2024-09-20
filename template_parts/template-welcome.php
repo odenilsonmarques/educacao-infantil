8e32dd4f54cbd3847d98c450d7c7ce6197792ead
odenilson marques <odmarquesgm@gmail.com>
2024-09-19 23:44:20 -0300

    create intens of section welcome

--- template_parts/template-welcome.php
+++ template_parts/template-welcome.php
@@ -1,27 +1,107 @@
 <div class="container">
     <section class="welcome">
         <div class="row">
-            <div class="col-lg-12 p-3 text-center " >
+            <div class="col-lg-12 p-3 text-center ">
                 <h1 class="mt-5 mb-3 fs-2 fw-bolder">Bem-vindo ao time Kinter</h1>
                 <p class="mx-auto lh-base paragraph-custom">
                     Estamos animados em acompanhar o crescimento e desenvolvimento das nossas crianças, proporcionando um ambiente de aprendizado divertido e acolhedor.
                 </p>
             </div>
         </div>
-        <div class="row justify-content-center text-center">
+        <div class="row justify-content-center text-center mt-5">
             <div class="col-lg-3 col-md-6 col-sm-12 mb-3">
-                apredizado
+                <div class="image-custom-item-1 mb-3">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/f-icon-1.png" alt="icone-1" class="img-fluid rounded-circle" style="background-color:red;">
+                </div>
+                <h5 class="custom-border">Apredizado</h5>
+                <p>
+                    Desde que fui um parceiro de engenharia de software confiável e visionário, fui um parceiro de engenharia de software confiável e visionário.
+                </p>
             </div>
             <div class="col-lg-3 col-md-6 col-sm-12 mb-3">
-                leitura
+                <div class="image-custom-item-2 mb-3">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/f-icon-2.png" alt="icone-1" class="img-fluid">
+                </div>
+                <h5>Professores</h5>
+                <p>
+                    Desde que fui um parceiro de engenharia de software confiável e visionário, fui um parceiro de engenharia de software confiável e visionário.
+                </p>
             </div>
             <div class="col-lg-3 col-md-6 col-sm-12 mb-3">
-                ensino
+                <div class="image-custom-item-3 mb-3">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/f-icon-3.png" alt="icone-1" class="img-fluid">
+                </div>
+                <h5>Dia dos pais</h5>
+                <p>
+                    Desde que fui um parceiro de engenharia de software confiável e visionário, fui um parceiro de engenharia de software confiável e visionário.
+                </p>
             </div>
             <div class="col-lg-3 col-md-6 col-sm-12 mb-3">
-                pesquisa
+                <div class="image-custom-item-4 mb-3">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/f-icon-4.png" alt="icone-1" class="img-fluid">
+                </div>
+                <h5>Aulas de Música</h5>
+                <p>
+                    Desde que fui um parceiro de engenharia de software confiável e visionário, fui um parceiro de engenharia de software confiável e visionário.
+                </p>
             </div>
         </div>
     </section>
 </div>
 
+<style>
+    .image-custom-item-1 img {
+        width: 100px;
+        /* Tamanho da imagem */
+        height: 100px;
+        /* object-fit: cover; */
+        border-radius: 100%;
+        /* Força a imagem a ficar redonda */
+        padding: 15px;
+
+        background-image: linear-gradient(145deg, rgb(35, 204, 136) 0%, rgb(142, 207, 53) 100%);
+        box-shadow: 0px 18px 30px 0px rgba(38, 204, 133, 0.36);
+    }
+
+    .image-custom-item-2 img {
+        width: 100px;
+        /* Tamanho da imagem */
+        height: 100px;
+        /* object-fit: cover; */
+        border-radius: 100%;
+        /* Força a imagem a ficar redonda */
+        padding: 15px;
+
+        background-image: linear-gradient(145deg, rgb(255, 79, 88) 0%, rgb(255, 180, 0) 100%);
+        box-shadow: 0px 18px 24px 0px rgba(255, 114, 58, 0.36);
+
+    }
+
+    .image-custom-item-3 img {
+        width: 100px;
+        /* Tamanho da imagem */
+        height: 100px;
+        /* object-fit: cover; */
+        border-radius: 100%;
+        /* Força a imagem a ficar redonda */
+        padding: 15px;
+
+        background-image: linear-gradient(145deg, rgb(1, 203, 173) 0%, rgb(71, 161, 242) 100%);
+        box-shadow: 0px 18px 24px 0px rgba(1, 203, 173, 0.36);
+
+    }
+
+    .image-custom-item-4 img {
+        width: 100px;
+        /* Tamanho da imagem */
+        height: 100px;
+        /* object-fit: cover; */
+        border-radius: 100%;
+        /* Força a imagem a ficar redonda */
+        padding: 15px;
+
+        background-image: linear-gradient(145deg, rgb(232, 90, 202) 0%, rgb(252, 78, 101) 100%);
+        box-shadow: 0px 18px 24px 0px rgba(233, 89, 197, 0.36);
+
+    }
+</style>
