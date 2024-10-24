f911f8100da90e372ba3fde755d5bd971fbf185b
odenilson marques <odmarquesgm@gmail.com>
2024-10-24 17:41:35 -0300

    update content sof section class

--- functions.php
+++ functions.php
@@ -74,3 +74,58 @@ function educacao_infantil_sidebars()
     
 }
 add_action('widgets_init', 'educacao_infantil_sidebars');
+
+
+//função para registrar o Custom Post Type (CPT)
+function create_turmas_cpt(){
+
+    // Array para definir os rótulos (labels) que serão exibidos no painel adm
+    $labels = array(
+        'name' => _x('Turmas', 'Post Type General Name', 'textdomain'),
+        'singular_name' => _x('Turma', 'Post Type Singular Name', 'textdomain'),
+        'menu_name' => __('Turmas', 'textdomain'),
+        'name_admin_bar' => __('Turma', 'textdomain'),
+        'archives' => __('Arquivo de Turmas', 'textdomain'),
+        'attributes' => __('Atributos de Turmas', 'textdomain'),
+        'parent_item_colon' => __('Turma Parente:', 'textdomain'),
+        'all_items' => __('Todas as Turmas', 'textdomain'),
+        'add_new_item' => __('Adicionar Nova Turma', 'textdomain'),
+        'add_new' => __('Adicionar Nova', 'textdomain'),
+        'new_item' => __('Nova Turma', 'textdomain'),
+        'edit_item' => __('Editar Turma', 'textdomain'),
+        'update_item' => __('Atualizar Turma', 'textdomain'),
+        'view_item' => __('Ver Turma', 'textdomain'),
+        'view_items' => __('Ver Turmas', 'textdomain'),
+        'search_items' => __('Procurar Turmas', 'textdomain'),
+        'not_found' => __('Não Encontrado', 'textdomain'),
+        'not_found_in_trash' => __('Não Encontrado no Lixo', 'textdomain'),
+    );
+
+    // Este array define as configurações e o comportamento do Custom Post Type
+    $args = array(
+        'label' =>__('Turma', 'textdomain'),
+        'description' =>__('Custom POst Type para tipos de turmas', 'textdomain'),
+        'labels' => $labels,
+        'supports' => array('title', 'editor', 'thumbnail', 'excerpt', 'custom-fields'),
+        'hierarchical' => false,
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'menu_position' => 5,
+        'menu_icon' => 'dashicons-welcome-learn-more',
+        'show_in_admin_bar' => true,
+        'show_in_nav_menus' => true,
+        'can_export' => true,
+        'has_archive' => true,
+        'exclude_from_search' => false,
+        'publicly_queryable' => true,
+        'capability_type' => 'post',
+
+    );
+
+    register_post_type('turmas', $args);
+}
+
+
+
+add_action('init', 'create_turmas_cpt');
--- template_parts/template-classes.php
+++ template_parts/template-classes.php
@@ -8,35 +8,63 @@
                 </p>
             </div>
         </div>
-        <div class="row justify-content-center text-center mt-5">
-            <div class="col-lg-4 col-md-6 col-sm-12 mb-3 widget-image-custom d-flex flex-column align-items-center shadow p-3 mb-5 bg-body rounded">
-                <div>
-                    <?php if (is_active_sidebar('classe-infantil-um')) : ?>
-                        <div id="class-infantil">
-                            <?php dynamic_sidebar('classe-infantil-um'); ?>
-                        </div>
-                    <?php endif; ?>
+        <div class="row justify-content-center mt-5">
 
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 mb-3 widget-image-custom d-flex flex-column align-items-center shadow p-3 mb-5 bg-body rounded">
-                <div>
-                    <?php if (is_active_sidebar('classe-infantil-dois')) : ?>
-                        <div id="class-infantil">
-                            <?php dynamic_sidebar('classe-infantil-dois'); ?>
+
+            <?php
+            $args = array(
+                'post_type' => 'turmas',
+                'posts_per_page' => -1,
+            );
+            $turmas_query = new WP_Query($args);
+            if ($turmas_query->have_posts()) :
+                $counter = 0; // contador para alternar entre esquerda e direita
+                while ($turmas_query->have_posts()) : $turmas_query->the_post(); ?>
+                    <?php if ($counter % 2 == 0) : // Para a primeira turma e as subsequentes em posição par 
+                    ?>
+                        <div class="row mb-5 mt-3">
+                            <div class="col-6 text-end">
+                                <?php if (has_post_thumbnail()) : ?>
+                                    <img src="<?php the_post_thumbnail_url('large'); ?>" alt="<?php the_title(); ?>" class="img-fluid rounded custom-image-size me-3">
+                                <?php endif; ?>
+                            </div>
+                            <div class="col-5 align-self-center">
+                                <div class="ms-5">
+                                    <h2 class="text-primary"><?php the_title(); ?></h2>
+                                    <p class="" style="max-width: 50%;"><?php the_excerpt(); ?></p>
+                                    <a href="<?php the_permalink(); ?>" class="btn btn-warning text-white">Agende sua visita!</a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php else : // Para as turmas em posição ímpar 
+                    ?>
+                        <div class="row mb-5 mt-3">
+                            <div class="col-1"></div>
+
+                            <div class="col-5 align-self-center">
+                                <div class="ms-5">
+                                    <h2 class="text-primary"><?php the_title(); ?></h2>
+                                    <p><?php the_excerpt(); ?></p>
+                                    <a href="<?php the_permalink(); ?>" class="btn btn-warning text-white">Agende sua visita!</a>
+                                </div>
+                            </div>
+
+                            <div class="col-6">
+                                <?php if (has_post_thumbnail()) : ?>
+                                    <img src="<?php the_post_thumbnail_url('large'); ?>" alt="<?php the_title(); ?>" class="img-fluid rounded custom-image-size ms-5">
+                                <?php endif; ?>
+                            </div>
                         </div>
                     <?php endif; ?>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 mb-3 widget-image-custom d-flex flex-column align-items-center shadow p-3 mb-5 bg-body rounded">
-                <div>
-                    <?php if (is_active_sidebar('classe-infantil-tres')) : ?>
-                        <div id="class-infantil">
-                            <?php dynamic_sidebar('classe-infantil-tres'); ?>
-                        </div>
-                    <?php endif; ?>
-                </div>
-            </div>
+                    <?php $counter++; // Incrementa o contador 
+                    ?>
+            <?php endwhile;
+                wp_reset_postdata();
+            else :
+                echo '<p>Nenhuma turma encontrada.</p>';
+            endif;
+            ?>
+
         </div>
     </div>
 </section>
