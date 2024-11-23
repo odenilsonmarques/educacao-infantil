ac7359d5f817463d3127dd96bea25d6e66a147be
odenilson marques <odmarquesgm@gmail.com>
2024-11-23 17:21:02 -0300

    update section classes and our numbers

--- template_parts/template-ours-numbers.php
+++ template_parts/template-ours-numbers.php
@@ -3,7 +3,7 @@
         <!-- use aqui uma mt-5 porque nao tenho um h1 nesse a section -->
         <div class="row justify-content-center text-center">
             <div class="col-lg-4 col-md-6 col-sm-12 mb-5 mt-5  d-flex flex-column align-items-center">
-                <div class="mt-5 mb-2">
+                <div class="mt-5 mb-3">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/alunos2.png" alt="">
                 </div>
                 <h3 class="fs-1 fw-bolder text-white">
@@ -12,7 +12,7 @@
                 <p class="fs-5 fw-bolder text-white mb-5">Alunos matriculados</p>
             </div>
             <div class="col-lg-4 col-md-6 col-sm-12 mb-5 mt-5  d-flex flex-column align-items-center">
-                <div class="mt-5 mb-2">
+                <div class="mt-5 mb-3">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/parabens.png" alt="">
                 </div>
                 <h3 class="fs-1 fw-bolder text-white">
@@ -21,7 +21,7 @@
                 <p class="fs-5 fw-bolder text-white mb-5">Alunos formados</p>
             </div>
             <div class="col-lg-4 col-md-6 col-sm-12 mb-5 mt-5  d-flex flex-column align-items-center">
-                <div class="mt-5 mb-2">
+                <div class="mt-5 mb-3">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/professora.png" alt="">
                 </div>
                 <h3 class="fs-1 fw-bolder text-white">
@@ -32,5 +32,3 @@
         </div>
     </div>
 </section>
-
-
